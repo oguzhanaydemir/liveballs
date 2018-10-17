8d7f50ca82956cb0b139043437370e7792073b3b
oguzhanaydemir <ouzhanaydemir7@gmail.com>
2018-10-17 17:03:26 +0300

    Add newUser socket  to project

--- public/javascripts/controllers/indexController.js
+++ public/javascripts/controllers/indexController.js
@@ -1,19 +1,36 @@
 app.controller("indexController", [
   "$scope", "indexFactory",
   ($scope, indexFactory) => {
-    
-    const connectionOptions = {
-			reconnectionAttempts: 3,
-			reconnectionDelay: 600
-    };
-    
-    indexFactory.connectSocket('http://localhost:3000', connectionOptions)
-    .then((socket) => {
-      console.log(socket);      
-    })
-    .catch((err) => {
-      console.log(err);
-    });
 
+    $scope.init = () => {
+      let username;
+
+      let usernameControl = false;
+
+      while (!usernameControl) {
+        username = prompt('Please enter a username');
+        if (username)
+          usernameControl = true;
+      }
+
+      initSocket(username);
+    }
+
+
+    function initSocket(username) {
+
+      const connectionOptions = {
+        reconnectionAttempts: 3,
+        reconnectionDelay: 600
+      };
+
+      indexFactory.connectSocket('http://localhost:3000', connectionOptions)
+        .then((socket) => {
+          socket.emit('newUser', { username });
+        })
+        .catch((err) => {
+          console.log(err);
+        });
+    }
   }
 ]);
